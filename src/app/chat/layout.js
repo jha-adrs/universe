9c7c5721c7120c9decd7f697dd7c6e9544eada0b
Aadarsh Jha <88365910+jha-adrs@users.noreply.github.com>
2025-06-05 00:30:39 +0530

    more changes

--- src/app/chat/layout.js
+++ src/app/chat/layout.js
@@ -1,11 +1,9 @@
-
 import { getAuthSession } from '@/lib/auth'
 
 
 const Layout =async ({children, params}) => {
-    
     // Fetch the current user
-    
+    const session = await getAuthSession()
 
   return (
     <>
